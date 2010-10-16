378987baafa555528c28c405a0ad18e5a44b0949
Simon Whitaker <sw@netcetera.org>
2010-10-16 21:56:02 +0100

    Updating to use nanoc

---
+++ src/lib/navlinks.rb
@@ -0,0 +1,9 @@
+def navlinks
+    return [
+        { :text => "Home",      :url => "/" },
+        { :text => "Contact",   :url => "/contact/" },
+        { :text => "About",     :url => "/about/" },
+        { :text => "Help",      :url => "/help/" },
+        { :text => "Blog",      :url => "http://blog.goosoftware.co.uk/" },
+    ]
+end
