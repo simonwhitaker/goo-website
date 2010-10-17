aeefdfeba71885c7e0393cc710123826aad19fee
Simon Whitaker <sw@netcetera.org>
2010-10-17 15:13:51 +0100

    Front page pimping

--- src/content/js/showcase.js
+++ src/content/js/showcase.js
@@ -1,37 +1,54 @@
-$(document).ready(function(){
-  ul = $('<ul id="app-tabs"></ul>');
-  ul.addClass('tabs');
-  
-  // populate tabs
-  $('#apps .app').each(function(index){
-    name = $(this).attr('id'); // e.g. "foo-bar"
-    title = $(this).find('h2').find('a').html(); // e.g. "Foo Bar"
-    a = $('<a>' + title + '</a>');
-    a.attr('href', 'javascript:void(0);');
-    a.attr('rel', name);
-    
-    a.click(function(event) {
-      // display appropriate .app block
-      $('.app[display!=none]').each(function(){$(this).hide()});
-      $('#' + $(this).attr('rel')).fadeIn();
+function get_current_page() {
+    return Math.round($('#apps').scrollLeft() / $('#apps').width());
+}
 
-      // selet the appropriate tab
-      $('#app-tabs').find('.selected').each(function(){$(this).removeClass('selected')});
-      $('#app-tabs .tab a').eq(index).addClass('selected');
-      event.preventDefault();
+function scroll_to_page(page, animate) {
+    var page_width =  $('#apps').width();
+    var prev_page = get_current_page()
+    var attr = {'scrollLeft': page_width * page};
+    // Do the scrolling
+    if (animate) {
+        $('#apps').animate(attr, 300);
+    } else {
+        $('#apps').attr(attr);
+    }
+    $('.pager').each(function(index){
+        $(this).toggleClass('selected', index == page);
     });
-    
-    li = $('<li></li>');
-    li.addClass('tab');
-    if (index == 0)
-      a.addClass('selected');
+}
 
-    // title.appendTo(a);
-    a.appendTo(li);
-    li.appendTo(ul);
-  });
-  
-  ul.prependTo('#apps');
-  ul.find('li').first().find('a').addClass('first');
-  ul.find('li').last().find('a').addClass('last');
+$(function () {
+    var w = $('#apps').width();
+    
+    // don't run on the iPhone
+    if (w > 480) {
+        var page_control = $('<div></div>').attr('id','page_control');
+        var PADDING = 30;
+        $('.app').each(function(index) {
+            $(this).css({
+                'position': 'absolute',
+                'top':      0,
+                'left':     (w * index)  + 'px',
+                'width':    (w - PADDING * 2)  + 'px',
+                'padding':  PADDING + 'px',
+            });
+            var pager = $('<a></a>').attr('class','pager');
+            pager.attr('href','#!' + $(this).attr('id')).html($('h2 a', $(this)).html());
+            pager.click(function(){ scroll_to_page(index, true); });
+            page_control.append(pager);
+        });
+        $('#apps').css('overflow','hidden');
+        $('#apps').css('height', '480px');
+        $('#showcase').prepend(page_control);
+    }
+    if (r = location.href.match('#!(.+)')) {
+        var app = $('#' + r[1]);
+        var index = $('.app').index(app);
+        if (index >= 0)
+            scroll_to_page(index, false);
+        else
+            scroll_to_page(0);
+    } else {
+        scroll_to_page(0);
+    }
 });
