d0b5c67d1ce989e26ce886935129c9668fd1e29c
Simon Whitaker <simon@goosoftware.co.uk>
2011-09-09 13:03:09 +0100

    Further tweaks

--- src/lib/navlinks.rb
+++ src/lib/navlinks.rb
@@ -3,7 +3,7 @@ def navlinks
         { :text => "Home",      :url => "/" },
         { :text => "Contact",   :url => "/contact/" },
         { :text => "About",     :url => "/about/" },
-        { :text => "Help",      :url => "/help/" },
+        # { :text => "Help",      :url => "/help/" },
         { :text => "Blog",      :url => "/blog/" },
     ]
 end
