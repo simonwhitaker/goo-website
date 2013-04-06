59703731591e824b1eb692d0860be49af3f63cdb
Simon Whitaker <simon@goosoftware.co.uk>
2013-04-06 23:04:35 +0100

    Update Goohub link

--- src/lib/navlinks.rb
+++ src/lib/navlinks.rb
@@ -3,7 +3,7 @@ def navlinks
         { :text => "Home",      :url => "/" },
         { :text => "Contact",   :url => "/contact/" },
         { :text => "About",     :url => "/about/" },
-        { :text => "GooHub",    :url => "http://goosoftware.github.com/" },
+        { :text => "GooHub",    :url => "http://goosoftware.github.io/" },
         { :text => "Blog",      :url => "http://blog.goosoftware.co.uk/" },
     ]
 end
